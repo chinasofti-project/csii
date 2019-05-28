16fe00538f27b29336558e504ff0edf35e9fcab0
sky099 <379128589@qq.com>
2019-05-28 18:24:30 +0800

    sql re-structure

---
+++ doc/db/chinasoft_data.sql
@@ -0,0 +1,23 @@
+INSERT INTO `basic` (`id`, `step`, `status`, `department`, `hsbc_bu`, `hr`, `rm`, `csi_interviewer`, `csi_interview_email`, `hsbc_interviewer`, `hsbc_interviewer_email`, `cn_name`, `en_name`, `gender`, `phone`, `home_province`, `home_city`, `work_location`, `card_number`, `university`, `degree`, `major`, `graduation_date`, `skills`, `certification`, `lead_experience`, `mf_as400`, `change_job`, `create_time`, `update_time`, `personal_email`) VALUES
+(18, '1', '1', 'mono', 'boc', '李雯', '陈-', 'Maggie', 'maggie@sina.com', 'Jason', 'jason@sina.com', '宋洋', 'James', '男', '13434567890', '3', '3', 'Guangzhou', '1243545464565767', '四川大学', 'master', '专业', '05/08/2019', 'java', 'CET-6', '未知', 'MF 2years', '个人原因', 1558078953, 1558078953, NULL),
+(20, '1', '1', 'mono', 'hcc', '惠子', '仝新轩', 'Samantha', 'samantha@163.com', 'Hailey', 'hailey@163.com', '涂悦兹', 'Lavelle', '女', '17728952368', '14', '2', 'Xi''an', '4565676768687867', '中山大学', 'bachelor', '文学', '05/01/2014', 'java', 'CET-4', '7', 'MF 3years', '无', 1558080427, 1558080427, NULL),
+(19, '1', '1', 'mono', 'ccs', '乔一', '金珂', 'David', 'david@126.com ', 'Tyler', 'tyler@126.com ', '辛欣', 'Elizabeth', '女', '18823456721', '1', '1', 'Xi''an', '5678888888888888', '北京大学', 'doctor', '计算机', '05/01/2014', 'mainframe,AS400', 'CET-6', '3', 'MF 3years', '个人原因', 1558079809, 1558079809, NULL),
+(24, '1', '1', 'mono', 'ccs', '乔一', '金珂', 'David', 'david@126.com ', 'Tyler', 'tyler@126.com ', '辛欣01', 'Elizabeth', '女', '18823456721', '1', '1', 'Xi''an', '5678888888888888', '北京大学', 'doctor', '计算机', '05/01/2014', 'mainframe,AS400', 'CET-6', '3', 'MF 3years', '个人原因', 1558079809, 1558079809, NULL),
+(25, '1', '1', 'mono', 'ccs', '乔一01', '金珂', 'David', 'david@126.com ', 'Tyler', 'tyler@126.com ', '辛欣01', 'Elizabeth', '女', '18823456721', '1', '1', 'Xi''an', '5678888888888888', '北京大学', 'doctor', '计算机', '05/01/2014', 'mainframe,AS400', 'CET-6', '3', 'MF 3years', '个人原因', 1558079809, 1558079809, NULL);
+
+insert into csiifb (id,cid,cn_name,speaking,listening,reading,writing,cantonese,exist_role,
+domain_knowledge,system_design,technical,technical_score,motivation,career_plan,expession,question_asked,
+finally_rating,work_date,result) values
+(null,1,'李军','3','3','3','3','3',DEFAULT,'3','3','JAVA','100','3','3','3','3','3','2019-5-13','PASS');
+
+insert into custfb (id,hid,cn_name,hsbc_interview_date,technical,lang,management,communication,
+domain,finally_result,reject_reason,requirement_for_next)values
+(null,1,'李军','2019-5-13','4','4','4','4','4','Pass',DEFAULT,DEFAULT);
+
+INSERT INTO `user` (`id`, `user_name`, `user_password`, `f_id`, `deparment`, `role_type`, `create_time`, `login_name`) VALUES
+(1, 'HR测试', '123456', 0, 'MNC', 'hr', '2019-05-25 13:14:00', 'hrmnc001'),
+(2, 'CSI测试', '123456', 0, 'MNC', 'csi', '2019-05-25 13:14:00', 'csimnc001'),
+(3, 'HSBC测试', '123456', 0, 'MNC', 'hsbc', '2019-05-25 13:14:00', 'hsbcmnc001'),
+(4, 'HR子账号测试1', '123456', 1, 'MNC', 'hr', '2019-05-28 13:14:00', 'hrmnc001'),
+(5, 'HR子账号测试2', '123456', 1, 'MNC', 'hr', '2019-05-28 13:14:00', 'hrmnc001'),
+(6, 'HR子账号测试3', '123456', 1, 'MNC', 'hr', '2019-05-28 13:14:00', 'hrmnc001');
---
+++ doc/db/chinasoft_script.sql
@@ -0,0 +1,91 @@
+create database csii_db charset=utf8;
+use csii_db;
+
+create table basic(
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  step enum('1','2','3') NOT NULL DEFAULT '1' COMMENT '1.初始录入、2.中软面试、3.客户面试',
+  status enum('1','2','3','4','5') NOT NULL DEFAULT '1' COMMENT '1-progressing、2-CSI-Pass/CSI-Failed、3-CUI-Pass/CUI-Failed',
+  personal_email varchar(225) DEFAULT NULL,
+  department VARCHAR(32) NOT NULL DEFAULT '-',
+  hsbc_bu VARCHAR(32) NOT NULL DEFAULT '-',
+  hr VARCHAR(8) NOT NULL DEFAULT '-',
+  rm VARCHAR(8) NOT NULL DEFAULT '-',
+  csi_interviewer VARCHAR(8) NOT NULL DEFAULT '-',
+  csi_interview_email VARCHAR(64) NOT NULL DEFAULT '-',
+  hsbc_interviewer VARCHAR(8) NOT NULL DEFAULT '-',
+  hsbc_interviewer_email VARCHAR(64) NOT NULL DEFAULT '-',
+  cn_name VARCHAR(8) NOT NULL DEFAULT '-',
+  en_name VARCHAR(32) NOT NULL DEFAULT '-',
+  gender VARCHAR(2) NOT NULL DEFAULT '-',
+  phone VARCHAR(16) NOT NULL DEFAULT '-',
+  home_province VARCHAR(8) NOT NULL DEFAULT '-',
+  home_city VARCHAR(8) NOT NULL DEFAULT '-',
+  work_location VARCHAR(16) NOT NULL DEFAULT '-',
+  card_number VARCHAR(16) NOT NULL DEFAULT '-',
+  university VARCHAR(32) NOT NULL DEFAULT '-',
+  degree VARCHAR(32) NOT NULL DEFAULT '-',
+  major VARCHAR(64) NOT NULL DEFAULT '-',
+  graduation_date VARCHAR(64) NOT NULL DEFAULT '-',
+  skills VARCHAR(64) NOT NULL DEFAULT '-',
+  certification VARCHAR(64) NOT NULL DEFAULT '-',
+  lead_experience VARCHAR(64) NOT NULL DEFAULT '-',
+  mf_as400 VARCHAR(64) NOT NULL DEFAULT '-',
+  change_job VARCHAR(64) NOT NULL DEFAULT '-',
+  create_time int(11) unsigned NOT NULL DEFAULT '0' COMMENT '创建时间',
+  update_time int(11) unsigned NOT NULL DEFAULT '0' COMMENT '更新时间'
+);
+
+
+create table csiifb(
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  cid INT,
+  cn_name VARCHAR(8) NOT NULL DEFAULT '-',
+  speaking VARCHAR(2) NOT NULL DEFAULT '-',
+  listening VARCHAR(2) NOT NULL DEFAULT '-',
+  reading VARCHAR(2) NOT NULL DEFAULT '-',
+  writing VARCHAR(2) NOT NULL DEFAULT '-',
+  cantonese VARCHAR(2) NOT NULL DEFAULT '-',
+  exist_role VARCHAR(128) NOT NULL DEFAULT '-',
+  domain_knowledge VARCHAR(2) NOT NULL DEFAULT '-',
+  system_design VARCHAR(2) NOT NULL DEFAULT '-',
+  technical VARCHAR(16) NOT NULL DEFAULT '-',
+  technical_score VARCHAR(8) NOT NULL DEFAULT '-',
+  motivation VARCHAR(2) NOT NULL DEFAULT '-',
+  career_plan VARCHAR(2) NOT NULL DEFAULT '-',
+  expession VARCHAR(2) NOT NULL DEFAULT '-',
+  question_asked VARCHAR(2) NOT NULL DEFAULT '-',
+  finally_rating VARCHAR(2) NOT NULL DEFAULT '-',
+  work_date VARCHAR(64) NOT NULL DEFAULT '-',
+  result VARCHAR(8) NOT NULL DEFAULT '-',
+  FOREIGN KEY (cid) REFERENCES basic_background_information(id)
+);
+
+create table custfb(
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  hid INT,
+  cn_name VARCHAR(8) NOT NULL DEFAULT '-',
+  hsbc_interview_date VARCHAR(64) NOT NULL DEFAULT '-',
+  technical VARCHAR(2) NOT NULL DEFAULT '-',
+  lang VARCHAR(2) NOT NULL DEFAULT '-',
+  management VARCHAR(2) NOT NULL DEFAULT '-',
+  communication VARCHAR(2) NOT NULL DEFAULT '-',
+  domain VARCHAR(2) NOT NULL DEFAULT '-',
+  finally_result VARCHAR(8) NOT NULL DEFAULT '-',
+  reject_reason VARCHAR(64) NOT NULL DEFAULT '-',
+  requirement_for_next VARCHAR(64) NOT NULL DEFAULT '-',
+  FOREIGN KEY (hid) REFERENCES basic_background_information(id)
+);
+
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '用户id',
+  `user_name` varchar(225) CHARACTER SET utf8 NOT NULL COMMENT '用户名',
+  `user_password` varchar(225) CHARACTER SET utf8 NOT NULL COMMENT '密码',
+  `f_id` int(11) NOT NULL COMMENT '父级id',
+  `deparment` varchar(225) CHARACTER SET utf8 NOT NULL COMMENT '部门',
+  `deparment_parent` varchar(225) CHARACTER SET utf8 NOT NULL COMMENT '部门',
+  `role_type` enum('hr','csi','hsbc') CHARACTER SET utf8 NOT NULL COMMENT '角色类型',
+  `create_time` datetime NOT NULL COMMENT '创建时间',
+  `login_name` varchar(225) CHARACTER SET utf8 NOT NULL COMMENT '登陆账号',
+  PRIMARY KEY (`id`)
+);
+
